b374978be9f0d2590a4f2d707c610a245e3cce13
deious <gkstjddlekd@gmail.com>
2020-06-17 22:56:51 +0900

    입력 감지 및 캐릭터 이동 스크립트 추가

---
+++ Survival/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string moveAxisName = "Vertical";
+    public string rotateAxisName = "Horizontal";
+    public string fireButtonName = "Fire";
+    public string reloadButtonName = "Reload";
+
+    public float move { get; private set; }
+    public float rotate { get; private set; }
+    public bool fire { get; private set; }
+    public bool reload { get; private set; }
+
+    void Update()
+    {
+        if(GameManager.instance != null && GameManager.instance.isGameover)
+        {
+            move = 0;
+            rotate = 0;
+            fire = false;
+            reload = false;
+            return;
+        }
+
+        move = Input.GetAxis(moveAxisName);
+        rotate = Input.GetAxis(rotateAxisName);
+        fire = Input.GetButton(fireButtonName);
+        reload = Input.GetButton(reloadButtonName);
+    }
+}
---
+++ Survival/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 180f;
+    private PlayerInput playerInput;
+    private Rigidbody playerRigidbody;
+    private Animator playerAnimator;
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Rotate();
+        Move();
+
+        playerAnimator.SetFloat("Move", playerInput.move);
+    }
+
+    void Move()
+    {
+        Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
+    }
+
+    void Rotate()
+    {
+        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
+        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
+    }
+}
