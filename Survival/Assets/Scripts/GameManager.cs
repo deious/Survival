0bcb3d0e3075d79e248dec1dfb978f8df0673a04
deious <gkstjddlekd@gmail.com>
2020-06-19 23:54:48 +0900

    게임매니저 스크립트 파일 추가

---
+++ Survival/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isGameOver;
+    int score = 0;
+    static GameManager m_instance;
+
+    public static GameManager instance
+    {
+        get
+        {
+            if(m_instance == null)
+            {
+                m_instance = FindObjectOfType<GameManager>();
+            }
+
+            return m_instance;
+        }
+    }
+
+    void Awake()
+    {
+        if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        //FindObjectOfType<PlayerHealth>().onDeath += EndGame;
+    }
+}
