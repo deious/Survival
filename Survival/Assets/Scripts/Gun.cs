cc615ef52ba9ea88a05533a5e323c07aefb7ca77
deious <gkstjddlekd@gmail.com>
2020-06-23 22:25:41 +0900

    총 관리 스크립트 추가

---
+++ Survival/Assets/Scripts/Gun.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public enum State
+    {
+        Ready,
+        Empty,
+        Reloading
+    }
+
+    public State state { get; set; }
+
+    public Transform fireTransform;
+
+    public ParticleSystem muzzleFlashEffect;
+    public ParticleSystem shellEjectEffect;
+
+    LineRenderer bulletLineRenderer;
+
+    AudioSource gunAudioPlayer;
+    public AudioClip shotClip;
+    public AudioClip reloadClip;
+
+    public float damage = 25;
+    float fireDistance = 50f;
+
+    public int ammoRemain = 100;
+    public int magCapacity = 25;
+    public int magAmmo;
+
+    public float timeBetFire = 0.12f;
+    public float reloadTime = 1.8f;
+    float lastFireTime;
+
+    void Awake()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        
+    }
+
+    public void Fire()
+    {
+
+    }
+
+    void Shot()
+    {
+
+    }
+
+    IEnumerator ShotEffect(Vector3 hitPosition)
+    {
+        bulletLineRenderer.enabled = true;
+
+        yield return new WaitForSeconds(0.03f);
+
+        bulletLineRenderer.enabled = false;
+    }
+
+    public bool Reload()
+    {
+        return false;
+    }
+
+    IEnumerator ReloadRoutine()
+    {
+        state = State.Reloading;
+
+        yield return new WaitForSeconds(reloadTime);
+
+        state = State.Ready;
+    }
+}
