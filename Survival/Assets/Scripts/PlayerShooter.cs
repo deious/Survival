e8bed6ac5689d07a381f3a3113413c2a2b1c26bf
deious <gkstjddlekd@gmail.com>
2020-06-28 23:45:03 +0900

    IK 관련 함수 추가

--- Survival/Assets/Scripts/PlayerShooter.cs
+++ Survival/Assets/Scripts/PlayerShooter.cs
@@ -42,11 +42,31 @@ public class PlayerShooter : MonoBehaviour
             }
         }
 
-        UpdateUI();
+        //UpdateUI();
     }
 
-    void UpdateUI()
+    /*void UpdateUI()               // UIManager 추가 후 적용 예정
     {
-        
+        if(gun != null && UIManager.instance != null)
+        {
+            UIManager.instance.UpdateAmmoText(gun.magAmmo, gun.ammoRemain);
+        }
+    }*/
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        gunPivot.position = playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow);
+
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandMount.rotation);
+
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.RightHand, rightHandMount.rotation);
     }
 }
